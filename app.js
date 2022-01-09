a84830b931627e66c7d9136e27eeb817f9c6e6b2
BalajiHarinath <89400019+BalajiHarinath@users.noreply.github.com>
2022-01-09 19:34:15 +0530

    Updated to ES6+ syntax

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-var btnTranslate = document.querySelector("#btn-translate");
-var txtInput = document.querySelector("#txtinput");
-var outputDiv = document.querySelector("#output-text");
+const btnTranslate = document.querySelector("#btn-translate");
+const txtInput = document.querySelector("#txtinput");
+const outputDiv = document.querySelector("#output-text");
 
-var serverURL = "https://api.funtranslations.com/translate/minion.json";
+const serverURL = "https://api.funtranslations.com/translate/minion.json";
 
-function getTranslatorURL(input){
+const getTranslatorURL = (input)=>{
     return serverURL + "?" + "text=" + input
 }
 
-function errorHandler(error){
+const errorHandler = (error)=>{
     console.log("error occured" + error);
     alert("Some issue with the server! please try after sometime")
 }
 
-function clickHandler(){
+const clickHandler = ()=>{
     console.log("clicked!")
-    var inputText = txtInput.value;
+    const inputText = txtInput.value;
     fetch(getTranslatorURL(inputText))
     .then(response => response.json())
     .then(json => {outputDiv.innerText = json.contents.translated;})
